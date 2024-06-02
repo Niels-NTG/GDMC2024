7f2696d0ab06ece7c9cbf55e746058255594cb90
Niels NTG <ntg999@gmail.com>
2024-06-02 19:49:01 +0200

    Add central staircase atrium structure

---
+++ structures/doha/central_core/central_core.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from glm import ivec3
+
+import globals
+from StructureBase import Structure
+
+
+class CentralCore(Structure):
+
+    def __init__(
+        self,
+        withRotation: int = 0,
+        tile: ivec3 = ivec3(0, 0, 0),
+        offset: ivec3 = ivec3(0, 0, 0),
+    ):
+        super().__init__(
+            structureFolder=globals.structureFolders[Path(__file__).parent.name],
+            withRotation=withRotation,
+            tile=tile,
+            offset=offset,
+        )
+
+    @property
+    def position(self) -> ivec3:
+        return self.offset + (self.tile * ivec3(5, 10, 5))
